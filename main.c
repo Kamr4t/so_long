c0f3655a84aac8d377be83dea4d6de87c1accd6e
Kamr4t <ancamara@c4a10c9.42berlin.de>
2025-06-17 15:31:49 +0200

    test

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ancamara <ancamara@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/16 12:21:03 by ancamara          #+#    #+#             */
-/*   Updated: 2025/06/17 13:06:57 by ancamara         ###   ########.fr       */
+/*   Updated: 2025/06/17 14:40:30 by ancamara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,6 +44,27 @@ int	ft_test(int key, void *param)
 	}
 	return (0);
 }
+
+int	ft_i_array_len(int *array)
+{
+	int	i;
+
+	i = 0;
+	while (array[i] != 0)
+		i++;
+	return (i);
+}
+
+int	ft_i_array_hight(int **array)
+{
+	int	i;
+
+	i = 0;
+	while (array[i] != 0)
+		i++;
+	return (1);
+}
+
 int	**ft_free_array(int **array, int hight)
 {
 	int	i;
@@ -58,11 +79,13 @@ int	**ft_free_array(int **array, int hight)
 	return (NULL);
 }
 
-int	*ft_map_store_line_int(int *map, size_t len)
+int	*ft_map_store_line_int(int *map)
 {
 	size_t	i;
+	size_t	len;
 	int		*array;
-
+	
+	len = ft_i_array_len(map);
 	 array = malloc(len * sizeof(int) + 1);
 	 if (!array)
 	 	return (NULL);
@@ -78,31 +101,29 @@ int	*ft_map_store_line_int(int *map, size_t len)
 
 int	*ft_map_store_line_char(char *map)
 {
-	size_t	len;
 	size_t	i;
+	size_t	len;
 	int		*array;
 
-	 len = ft_strlen(map);
-	 array = malloc(len * sizeof(int) + 1);
-	 if (!array)
-	 	return (NULL);
-	 i = 0;
-	 
-	 while (i < len)
-	 {
-		array[i] = map[i];
-		i++;
-	 }
-	 array[i] = 0;
-	 return (array);
+	len = ft_strlen(map);
+	array = malloc(len * sizeof(int) + 1);
+	if (!array)
+		return (NULL);
+	i = 0;
+	
+	while (i < len)
+	{
+	array[i] = map[i];
+	i++;
+	}
+	array[i] = 0;
+	return (array);
 }
-int	**ft_map_array(char *map, int hight, int **source_array)
+int	**ft_map_array(char *map, int hight, int **source_array, size_t len)
 {
 	int		**dest_array;
 	int		i;
-	size_t	len;
 
-	len = ft_strlen(map);
 	dest_array = malloc((hight + 1) * sizeof(int) + 1);
 	if (!dest_array)
 	{
@@ -112,7 +133,7 @@ int	**ft_map_array(char *map, int hight, int **source_array)
 	i = 0;
 	while (i < hight)
 	{
-		dest_array[i] = ft_map_store_line_int(source_array[i], len);
+		dest_array[i] = ft_map_store_line_int(source_array[i]);
 		if (!dest_array[i])
 			return (NULL);
 		free (source_array[i]);
@@ -128,9 +149,11 @@ int	**ft_map(int fd)
 {
 	int		**array;
 	int		hight;
+	size_t	len;
 	char	*map;
 
 	map = get_next_line(fd);
+	len = ft_strlen(map);
 	hight = 1;
 	array = malloc(hight * sizeof(int) + 1);
 	if (!array)
@@ -142,13 +165,58 @@ int	**ft_map(int fd)
 	map = get_next_line(fd);
 	while (map != NULL)
 	{
-		array = ft_map_array(map, hight, array);
+		array = ft_map_array(map, hight, array, len);
 		if (!array)
 			return (NULL);
 		map = get_next_line(fd);
 		hight++;
 	}
 	return (array);
+}
+int	ft_map_object_count(int c)
+{
+	static int	count_col = 0;
+	static int	count_player = 0;
+	static int	count_exit = 0;
+
+	if (c == 'C')
+		count_col++;
+	if (c == 'P')
+		count_player++;
+	if (c == 'E')
+		count_exit++;
+	if (c == 1)
+	{
+		if (count_col == 0)
+			return (0);
+		if (count_player != 1)
+			return (0);
+		if (count_exit != 1)
+			return (0);
+	}
+	return (1);
+}
+
+int	ft_map_wall(int **map)
+{
+	int		i;
+	int		j;
+	int		len;
+	int		hight;
+	
+	i = 0;
+	j = 0;
+	len = ft_i_array_len(map[0]);
+	hight = ft_i_array_hight(map);
+	while (i < len)
+	{
+		if (map[0][i] != '1')
+			return (0);
+		if (map[hight - 1][0] != '1')
+			return (0);
+		i++;
+	}
+	return (1);
 }
 
 int	ft_map_check(int **map)
@@ -162,17 +230,21 @@ int	ft_map_check(int **map)
 	while (map[i][j + 1] != 0)
 		j++;
 	len = j;
-	printf("%d\n", len);
-	while (map[i] != 0)
+		while (map[i] != 0)
 	{
 		j = 0;
 		while (map[i][j])
 		{
+			ft_map_object_count(map[i][j]);
 			j++;
 		}
+		if (j - 1 != len)
+			return (0);
 		i++;
 	}
-	return (0);
+	if (ft_map_object_count(1) == 0)
+		return (0);
+	return (1);
 }
 
 int	main(void)
@@ -186,9 +258,11 @@ int	main(void)
 	int		img_height;
 	int		fd = open("map1.ber", O_RDONLY);
 	int		**map;
+	int		test;
 
 	map = ft_map(fd);
-	ft_map_check(map);
+	test = ft_map_check(map);
+	ft_printf("MAP CHECK: %d\n", test);
 	if (!map)
 		return (0);
 	i = 0;
