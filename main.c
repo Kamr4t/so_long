84741b2150e98d0894ce5f2e00a1be3c464d63af
Kamr4t <anthony.camara.ac@googlemail.com>
2025-07-06 12:06:13 +0200

    valgrind fixes in main

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ancamara <ancamara@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/17 17:28:45 by ancamara          #+#    #+#             */
-/*   Updated: 2025/07/05 15:42:42 by ancamara         ###   ########.fr       */
+/*   Updated: 2025/07/06 12:03:02 by ancamara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,10 +24,16 @@ int	main(int argc, char **argv)
 	map = ft_map_array(fd, ft_map_file_hight(fd), argv[1]);
 	map_check = ft_map_check(map);
 	if (map_check == 0)
+	{
+		ft_free_map(map, ft_map_hight(map));
 		ft_invalid_map("The map does not follow the Rules!");
+	}
 	map_check = ft_map_dfs(fd, argv[1]);
 	if (map_check == 0)
+	{
+		ft_free_map(map, ft_map_hight(map));
 		ft_invalid_map("Not possible to reach all Objects!");
+	}
 	ft_build_win(map);
 	return (0);
 }
